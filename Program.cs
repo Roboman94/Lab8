26768d21cb979aaad2663b004868c15c7bbdb7f0
Kendall <kendallallmand@gmail.com>
2020-04-29 16:01:56 -0400

    first

--- Program.cs
+++ Program.cs
@@ -12,7 +12,8 @@ namespace Lab8
           bool tryagain = true;
           while (tryagain == true)
            {
-                    string[] names = { "Kendall", "Ryan", "Luke", "Corey" };
+                   
+                List<string> names = new List<string> { "Kendall", "Ryan", "Luke", "Corey" };
                     string[] indexArr = { "1", "2", "3", "4" };
                     string[] favoriteFoods = { "Steak", "Crab", "Fish", "Clams" };
                     string[] homeTown = { "Flint", "Compton", "Detroit", "Chicago" };
@@ -27,47 +28,72 @@ namespace Lab8
                     }
                     Console.WriteLine(name);
                 bool validentry = false;
+                string input = "";
+                    int index = 0;
                 while (validentry == false)
                 {
                     Console.WriteLine($"Please select name or number");
-                    string input = Console.ReadLine().ToLower();
-                    int index = 0;
-                    name = "";
+                    input = Console.ReadLine().ToLower();
+
                     if (input.Contains("kendall"))
                     {
+                        name = "Kendall";
                         index = 0;
+                        validentry = true;
                     }
-                    if (input.Contains("ryan"))
+                    else if (input.Contains("ryan"))
                     {
+                        name = "Ryan";
                         index = 1;
+                        validentry = true;
                     }
-                    if (input.Contains("luke"))
+                    else if (input.Contains("luke"))
                     {
+                        name = "Luke";
                         index = 2;
+                        validentry = true;
                     }
-                    if (input.Contains("corey"))
+                    else if (input.Contains("corey"))
                     {
+                        name = "Corey";
                         index = 3;
+                        validentry = true;
                     }
                     else
                     {
                         try
                         {
                             index = int.Parse(input);
-
-                            if (index >= 0 && index < names.Length)
+                            index = index - 1;
+                            if (index == 0)
                             {
-                                name = names[index - 1];
+                                validentry = true;
+                                name = "Kendall";
                             }
-                            else
+                            else if (index == 1)
                             {
-                                throw new Exception($"That number is outside the range of 0 to {names.Length - 1}. Please try again");
+                                validentry = true;
+                                name = "Ryan";
+                            }
+                            else if (index == 2)
+                            {
+                                validentry = true;
+                                name = "Luke";
+                            }
+                            else if (index == 3)
+                            {
+                                validentry = true;
+                                name = "Corey";
+                            }
+                            else if (index >= names.Count())
+                            {
+                                Console.WriteLine("Input exceeds index!");
                             }
                         }
                         catch (IndexOutOfRangeException)
                         {
-                            Console.WriteLine("You somehow skipped the first check..");
-                            Console.WriteLine("But the issue still remains, you entered a number outside of index range! Please try again.");
+                            
+                            Console.WriteLine("You entered a number outside of index range! Please try again.");
                         }
                         catch (FormatException)
                         {
@@ -75,13 +101,16 @@ namespace Lab8
                         }
                         catch (Exception e)
                         {
-                            ////writes any other err exception
                             Console.WriteLine(e.Message);
                         }
                     }
-                    if (name != "")
+                }
+                    if (input != "")
                     {
-                        Console.WriteLine($"You selected {name} at index: {index-1}");
+                    validentry = false;
+                    while (validentry == false)
+                        { 
+                        Console.WriteLine($"You selected {name} at index: {index}");
                         Console.WriteLine($"What would like to learn about {name}? type ff or favorite food ..");
                         Console.WriteLine("or type ht or home town.");
 
@@ -89,12 +118,12 @@ namespace Lab8
 
                         if (response == "ff" || response.Contains("food"))
                         {
-                            Console.WriteLine($"The favorite food of {name} is {favoriteFoods[index-1]}");
+                            Console.WriteLine($"The favorite food of {name} is {favoriteFoods[index]}");
                             validentry = true;
                         }
                         else if (response == "ht" || response.Contains("town"))
                         {
-                            Console.WriteLine($"The hometown of {name} is {homeTown[index-1]}");
+                            Console.WriteLine($"The hometown of {name} is {homeTown[index]}");
                             validentry = true;
                         }
                         else
@@ -102,10 +131,8 @@ namespace Lab8
                             Console.WriteLine("Invalid entry! Please try again!");
                             validentry = false;
                         }
-                    }
-
-
-                }
+                        }
+                    }  
              tryagain = Proceed();
           }
        }
